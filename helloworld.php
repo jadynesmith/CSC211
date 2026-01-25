b27b010fe55d554a61fe8d168a9ec9ee58147040
Jadyn Smith <jadynesmith.dev@gmail.com>
2026-01-25 01:06:31 -0700

    Added helloworld.php File
    Required for the "Basic PHP Syntax & Hello World" assignment.

---
+++ helloworld.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>
+            Hello, World!
+        </title>
+    </head>
+    <body>
+        <h1>
+            My First PHP Page
+        </h1>
+
+        <p>
+            The following was created by PHP:
+
+            <?php
+                echo "Hello, World!";
+            ?>
+        </p>
+    </body>
+</html>
