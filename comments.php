c940d56383bb2c10c38b7e984ccff19d5bd594db
Jadyn Smith <jadynesmith.dev@gmail.com>
2026-01-25 13:55:30 -0700

    Added comments.php File
    Required for the "Adding Comments to a Script" assignment.

---
+++ comments.php
@@ -0,0 +1,10 @@
+<?php
+    // This here is a single-line PHP comment.
+
+    # This is also a single-line PHP comment.
+
+    /*  This here is a multi-
+        line PHP comment. */
+?>
+
+<!-- This is a single-line HTML comment. -->
