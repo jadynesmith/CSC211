4a1839e11e74d52da9f771fd0e4807d86bc82e06
Jadyn Smith <jadynesmith.dev@gmail.com>
2026-01-27 11:59:45 -0700

    Added predefined.php File
    Required for the "Displaying a Variable's Value" assignment.

---
+++ predefined.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="uft-8">
+        <title>
+            Predefined Variables
+        </title>
+    </head>
+    <body>
+        <pre>
+            <?php
+                // get server and script information via $_SERVER function call
+                print_r($_SERVER);
+            ?>
+        </pre>
+    </body>
+</html>
