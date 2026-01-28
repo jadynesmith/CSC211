3c37b31c9c4c4f6e3c8913a8a303bf2aa0349843
Jadyn Smith <jadynesmith.dev@gmail.com>
2026-01-27 19:36:56 -0700

    Added feedback.html and handle_form.php Files
    Required for the "Creating a Simple HTML Form" assignment.

---
+++ handle_form.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="uft-8">
+        <title>
+            Feedback Submitted
+        </title>
+    </head>
+    <body>
+        <?php
+            print "You have submitted your feedback. Thank you.";
+        ?>
+    </body>
+</html>
