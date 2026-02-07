ef70931564848d374cbfbeeb3deadc47114fca8d
Jadyn Smith <jadynesmith.dev@gmail.com>
2026-02-06 22:45:24 -0700

    Added random_number.php File
    Required for the "Creating Random Numbers" Assignment.

---
+++ random_number.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>
+            Random Number
+        </title>
+    </head>
+    <body>
+        <?php
+            // generate a random number between 10 & 100
+            echo(rand(10, 100));
+        ?>
+    </body>
+</html>
