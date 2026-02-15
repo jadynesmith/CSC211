4af84683a03824c7e0527fa0edc6a2c92ed3588d
Jadyn Smith <jadynesmith.dev@gmail.com>
2026-02-15 12:55:21 -0700

    Added soups3.php File
    Required for the "Accessing & Printing Array Elements" Assignment.

---
+++ soups3.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>
+            No Soup for You!
+        </title>
+    </head>
+    <body>
+        <h1>
+            Mmm... soups
+        </h1>
+
+        <?php
+            // enable display errors
+            ini_set("display_errors", 1);
+
+            // set error reporting to highest level
+            error_reporting(E_ALL);
+            
+            // define & initialize array
+            $soups =
+            [
+                "Monday"    => "Clam Chowder",
+                "Tuesday"   => "White Chicken Chili",
+                "Wednesday" => "Vegetarian",
+                "Thursday"  => "Chicken Noodle",
+                "Friday"    => "Tomato",
+                "Saturday"  => "Cream of Broccoli",
+                "Sunday"    => "Wedding"
+            ];
+
+            // print menu title
+            print "<h3>Soups of the Week:</h3>";
+
+            // iterate through array
+            foreach ($soups as $day => $soup)
+            {
+                // print the day's soup option
+                print "<p>$day: $soup</p>\n";
+            }
+        ?>
+    </body>
+</html>
