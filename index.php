11f3e7ae2614ccd6013df9821253fe58f5b5f1e1
Jadyn Smith <jadynesmith.dev@gmail.com>
2026-02-22 16:38:34 -0700

    Added the index.php File
    Required for the "Using External Files" Assignment.

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    include("templates/header.html");
+?>
+<h2>
+    Welcome to a J.D. Salinger Fan Club
+</h2>
+
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipsicing elit, sed do eiusmod tempor incididunt ut labore et dolare magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+
+<?php
+    include("templates/footer.html");
+?>
