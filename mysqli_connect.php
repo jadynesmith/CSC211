cb04b359a638ba567e5e948212c9f3d363be8001
Jadyn Smith <jadynesmith.dev@gmail.com>
2026-03-06 21:56:14 -0700

    Added mysqli_connect.php File
    Required for the "Connecting to MySQL PHP Code" Assignment.

---
+++ mysqli_connect.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>
+            Connect to MySQL
+        </title>
+    </head>
+    <body>
+        <?php
+            // attempt database connection
+            if ($dbc = mysqli_connect("localhost", "root", "MyCSC211!", "myblog"))
+            {
+                // print success message
+                print "<p>Successfully connected to the database!</p>";
+
+                // close database connection
+                mysqli_close($dbc);
+            }
+            else
+            {
+                // print error message
+                print "<p style='color: red;'>Could not connect to the database.</p>";
+            }
+        ?>
+    </body>
+</html>
